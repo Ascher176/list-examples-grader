b68e4ba421395af163297b768baba30b50c5151c
Ascher176 <85227190+Ascher176@users.noreply.github.com>
2023-05-11 17:54:06 -0700

    -

--- grade.sh
+++ grade.sh
@@ -1,4 +1,5 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH1=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
+CPATH2=".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +15,20 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+set -e
+cd student-submission
+requiredFile="ListExamples.java"
+if [[ -f $requiredFile ]]
+then
+    echo "found a file"
+else
+    echo "missing a file"
+    exit
+fi
+cd ..
+cp ./student-submission/ListExamples.java grading-area 
+cp TestListExamples.java grading-area 
+
+javac -cp $CPATH1 grading-area/$requiredFile TestListExamples.java
+java -cp $CPATH2 org.junit.runner.JUnitCore TestListExamples
